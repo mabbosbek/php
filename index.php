8d91b8853397674ce618cba9cf278cafc1d70ed5
mabbosbek <mabbosbek@bk.ru>
2021-11-09 11:52:23 +0500

    update

--- index.php
+++ index.php
@@ -4,9 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>phpBlog - Home</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h1>Salom</h1>
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-sm-9">
+                <article>
+                    <h3>Lorem ipsum dolor sit amet.</h3>
+                    <p class="small-text">Pushlished: 09.11.2021 | Comments: 10 | Views: 27</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptatibus repellendus deleniti magni iure nihil vero, minus repudiandae facere quaerat, assumenda quisquam temporibus ex voluptatum ratione consequatur vel molestias at? 
+                        <a href="#" class="readmore">Read more</a>
+                    </p>
+                </article>
+                <hr>
+                <article>
+                    <h3>Lorem ipsum dolor sit amet.</h3>
+                    <p class="small-text">Pushlished: 09.11.2021 | Comments: 10 | Views: 27</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptatibus repellendus deleniti magni iure nihil vero, minus repudiandae facere quaerat, assumenda quisquam temporibus ex voluptatum ratione consequatur vel molestias at? 
+                        <a href="#" class="readmore">Read more</a>
+                    </p>
+                </article>
+                <hr>
+                <article>
+                    <h3>Lorem ipsum dolor sit amet.</h3>
+                    <p class="small-text">Pushlished: 09.11.2021 | Comments: 10 | Views: 27</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptatibus repellendus deleniti magni iure nihil vero, minus repudiandae facere quaerat, assumenda quisquam temporibus ex voluptatum ratione consequatur vel molestias at? 
+                        <a href="#" class="readmore">Read more</a>
+                    </p>
+                </article>
+                <hr>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-header">Search</div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-primary" type="button" id="button-addon2">GO</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="card mt-3 border-primary">
+                    <div class="card-header bg-primary text-light">Categories</div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><a href="#">HTML lessons</a></li>
+                            <li class="list-group-item"><a href="#">CSS lessons</a></li>
+                            <li class="list-group-item"><a href="#">PHP lessons</a></li>
+                            <li class="list-group-item"><a href="#">MySQL lessons</a></li>
+                            <li class="list-group-item"><a href="#">Bootstrap lessons</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
